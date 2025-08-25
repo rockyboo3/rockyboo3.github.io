14d209cfa75aa3777c77c3c8f4b5ffda3bfa0281
rockyboo3 <caitlyn.parrishlewis@gmail.com>
2025-08-24 22:40:24 -0400

    further mobile refining + buttons for projects

--- Projectscontent.js
+++ Projectscontent.js
@@ -8,7 +8,9 @@ $.get("/projects_info.json", (data) => {
                     '<strong style="font-size: 12px;">' + project["header"] + '<br>' +
                         '<i style="font-size: 11px;">' + project["subheader"] + '</i></strong><br>' +
                         project["description"] + '<br>' +
-                        '- <strong>' + project["project_link"] + '</strong><br><br>'
+                        (project["project_link"] !== "N/A" ? 
+                            '<a class="projects_buttons" target="_blank" href="' + project["project_link"] + '">PROJECT LINK</a>' : 
+                            '') + '<p id="' + project["header"] + '" class="projects_buttons gallery_link">GALLERY</p>' + '<br><br>'
         );
     });
 });
